7ed0a6bb81d21d149aebe7433b14fdcb4378a102
Dawei <corgiflash@users.noreply.github.com>
2018-10-02 21:58:48 -0700

    Flex Sensor
    Using the caculation to get the range of resistance from sensor.
    The degree may varies from different postitions. To test the degree changing, please make the sensor stablized before connceted the circuits.

---
+++ flexSensor/flexSensor.ino
@@ -0,0 +1,31 @@
+const int FLEX_PIN = A9; // Pin connected to voltage divider output
+const float VCC = 5; // Measured voltage of Ardunio 5V line
+const float R_DIV = 47500.0; // Measured resistance of 3.3k resistor
+
+const float STRAIGHT_RESISTANCE = 37300.0; // resistance when straight
+const float BEND_RESISTANCE = 90000.0; // resistance at 90 deg
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(FLEX_PIN, INPUT);
+}
+
+void loop() 
+{
+  // Read the ADC, and calculate voltage and resistance from it
+  int flexADC = analogRead(FLEX_PIN);
+  float flexV = flexADC * VCC / 1023.0;
+  float flexR = R_DIV * (VCC / flexV - 1.0);
+  Serial.println("Resistance: " + String(flexR) + " ohms");
+
+  // Use the calculated resistance to estimate the sensor's
+  // bend angle:
+  float angle = map(flexR, STRAIGHT_RESISTANCE, BEND_RESISTANCE,
+                   0, 90.0);
+  Serial.println("Bend: " + String(angle) + " degrees");
+  Serial.println();
+
+  delay(500);
+}
+
